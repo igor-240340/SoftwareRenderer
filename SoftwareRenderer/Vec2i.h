9760d875c3891469250bbdb9685533914c2be272
igor <igor.240340@gmail.com>
2024-10-07 02:15:46 +0700

    Implement triangle rendering

---
+++ SoftwareRenderer/Vec2i.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Vec2i
+{
+public:
+    int x;
+    int y;
+};
+
--- SoftwareRenderer/main.cpp
+++ SoftwareRenderer/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <format>
+#include <chrono>
+#include <thread>
 
 #include <SFML/Graphics.hpp>
 
@@ -7,19 +9,37 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
+#include "Vec2i.h"
+
 constexpr int w = 1024;
 constexpr int h = 768;
 
-void draw_line(int x0, int y0, int x1, int y1, sf::VertexArray& frame_buffer, const sf::Color& color);
+std::vector<int> interpolate_x(Vec2i a, Vec2i b);
+void draw_line(Vec2i a, Vec2i b, sf::VertexArray& frame_buffer, const sf::Color& color);
+void draw_triangle(Vec2i a, Vec2i b, Vec2i c, sf::VertexArray& frame_buffer, const sf::Color& color, bool filled = false);
 bool import_model_and_draw(const std::string& modelpath, sf::VertexArray& frame_buffer);
+
+sf::RenderWindow* p_win;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(w, h), "Software Renderer From Scratch");
-
+    sf::RenderWindow window(sf::VideoMode(w, h), "Software Renderer");
+    p_win = &window;
     sf::VertexArray frame_buffer(sf::Points, w * h);
 
-    import_model_and_draw("data/rubber_duck/scene.gltf", frame_buffer);
+    //import_model_and_draw("data/rubber_duck/scene.gltf", frame_buffer);
+
+    Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+    draw_triangle(t0[0], t0[1], t0[2], frame_buffer, sf::Color::Red, true);
+    draw_triangle(t0[0], t0[1], t0[2], frame_buffer, sf::Color::Red);
+
+    Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+    draw_triangle(t1[0], t1[1], t1[2], frame_buffer, sf::Color::White, true);
+    draw_triangle(t1[0], t1[1], t1[2], frame_buffer, sf::Color::White);
+
+    Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+    draw_triangle(t2[0], t2[1], t2[2], frame_buffer, sf::Color::Green, true);
+    draw_triangle(t2[0], t2[1], t2[2], frame_buffer, sf::Color::Green);
 
     while (window.isOpen())
     {
@@ -30,33 +50,71 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(frame_buffer);
-        window.display();
+        //window.clear();
+        //window.draw(frame_buffer);
+        //window.display();
     }
 
     return 0;
 }
 
-void draw_line(int x0, int y0, int x1, int y1, sf::VertexArray& frame_buffer, const sf::Color& color) {
-    bool swapped = false;
-    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+std::vector<int> interpolate_x(Vec2i a, Vec2i b)
+{
+    std::vector<int> x_coords;
+
+    const int total_height = b.y - a.y;
+    for (int y = a.y; y <= b.y; y++)
     {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
+        float y_way_percent = (y - a.y) / (float)total_height;
+        const int x = a.x + (b.x - a.x) * y_way_percent;
+        x_coords.push_back(x);
+    }
+
+    // TODO: Посмотреть, как повлияет на производительность std::move().
+    return x_coords;
+}
+
+void draw_line(Vec2i a, Vec2i b, sf::VertexArray& frame_buffer, const sf::Color& color) {
+    if (a.x == b.x && a.y == b.y)
+    {
+        std::cout << std::format("Line drawing from [{},{}] to [{},{}]\n", a.x, a.y, b.x, b.y);
+
+        sf::Vector2f pos(a.x, a.y);
+        int index = w * a.y + a.x; // Раскладываем строки буфера в горизонтальную линию.
+
+        pos.y = h - pos.y;
+        frame_buffer[index].position = pos;
+        frame_buffer[index].color = color;
+
+        p_win->draw(frame_buffer);
+        p_win->display();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        return;
+    }
+
+    bool swapped = false;
+    if (std::abs(a.x - b.x) < std::abs(a.y - b.y))
+    {
+        std::swap(a.x, a.y);
+        std::swap(b.x, b.y);
         swapped = true;
     }
 
-    if (x0 > x1)
+    if (a.x > b.x)
     {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
+        std::swap(a.x, b.x);
+        std::swap(a.y, b.y);
     }
 
-    for (int x = x0; x <= x1; x++)
+    if (swapped)
+        std::cout << std::format("Line drawing from [{},{}] to [{},{}]\n", a.y, a.x, b.y, b.x);
+    else
+        std::cout << std::format("Line drawing from [{},{}] to [{},{}]\n", a.x, a.y, b.x, b.y);
+
+    for (int x = a.x; x <= b.x; x++)
     {
-        float x_way_percent = (x - x0) / (float)(x1 - x0);
-        int y = (y1 - y0) * x_way_percent + y0;
+        float x_way_percent = (x - a.x) / (float)(b.x - a.x);
+        int y = (b.y - a.y) * x_way_percent + a.y;
 
         sf::Vector2f pos(x, y);
         int index = w * y + x; // Раскладываем строки буфера в горизонтальную линию.
@@ -69,8 +127,13 @@ void draw_line(int x0, int y0, int x1, int y1, sf::VertexArray& frame_buffer, co
             index = w * x + y; // Был своп координат. Меняем, чтобы не перепутать строки со столбцами.
         }
 
+        pos.y = h - pos.y; // В виртуальной СК начало координат в левом нижнем углу, а в мировой - в левом верхнем.
         frame_buffer[index].position = pos;
         frame_buffer[index].color = color;
+
+        p_win->draw(frame_buffer);
+        p_win->display();
+        //std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
@@ -115,9 +178,57 @@ bool import_model_and_draw(const std::string& modelpath, sf::VertexArray& frame_
             //const int b_screen_y = (vb.z * h) - 1;
             //std::cout << std::format("b_screen({},{})", b_screen_x, b_screen_y) << std::endl;
 
-            draw_line(a_screen_x, (h - a_screen_y) - 1.0f, b_screen_x, (h - b_screen_y) - 1.0f, frame_buffer, sf::Color::White);
+            Vec2i a(a_screen_x, a_screen_y);
+            Vec2i b(b_screen_x, b_screen_y);
+            draw_line(a, b, frame_buffer, sf::Color::White);
         }
     }
 
     return true;
+}
+
+void draw_triangle(Vec2i a, Vec2i b, Vec2i c, sf::VertexArray& frame_buffer, const sf::Color& color, bool filled)
+{
+    if (!filled)
+    {
+        draw_line(a, b, frame_buffer, color);
+        draw_line(b, c, frame_buffer, color);
+        draw_line(c, a, frame_buffer, color);
+        return;
+    }
+
+    // a=min(a.y,b.y,c.y).
+    // c=max(a.y,b.y,c.y).
+    if (a.y > b.y) std::swap(a, b);
+    if (a.y > c.y) std::swap(a, c);
+    if (b.y > c.y) std::swap(b, c);
+
+    // Координаты x сторон треугольника.
+    const std::vector<int> x_coords_ac = interpolate_x(a, c);
+    std::cout << "count(ac): " << x_coords_ac.size() << std::endl;
+
+    // Последний элемент ab равен первому элементу bc - удаляем его,
+    // чтобы размеры ac и ab+bc были равны.
+    std::vector<int> x_coords_ab = interpolate_x(a, b);
+    x_coords_ab.pop_back();
+    const std::vector<int> x_coords_bc = interpolate_x(b, c);
+    std::cout << "count(ab+bc): " << x_coords_ab.size() + x_coords_bc.size() << std::endl;
+    std::vector<int> x_coords_abc = x_coords_ab; // TODO: std::move().
+    x_coords_abc.insert(x_coords_abc.end(), x_coords_bc.begin(), x_coords_bc.end());
+
+    for (const auto& x : x_coords_abc)
+    {
+        std::cout << x << std::endl;
+    }
+
+    int triangle_height = c.y - a.y;
+    std::cout << triangle_height << std::endl;
+
+    for (int i = 0; i < triangle_height + 1; i++) {
+        const int x_1 = x_coords_ac[i];
+        const int x_2 = x_coords_abc[i];
+        Vec2i p1(x_1, i + a.y);
+        Vec2i p2(x_2, i + a.y);
+        draw_line(p1, p2, frame_buffer, color);
+    }
 }
