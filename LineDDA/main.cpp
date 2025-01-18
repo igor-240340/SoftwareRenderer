b7c300a02a32b3b986de0282ac4333f7b49f4c0c
igor <igor.240340@gmail.com>
2025-01-18 23:58:14 +0700

    Refactor line drawing
    Finished.

--- LineDDA/main.cpp
+++ LineDDA/main.cpp
@@ -17,6 +17,14 @@ void draw_line_3(std::vector<sf::Uint8>& frame_buffer);
 void draw_line_4(std::vector<sf::Uint8>& frame_buffer);
 void draw_line_5(std::vector<sf::Uint8>& frame_buffer);
 void draw_line_6(std::vector<sf::Uint8>& frame_buffer);
+void draw_line_7(std::vector<sf::Uint8>& frame_buffer);
+void draw_line_8(std::vector<sf::Uint8>& frame_buffer);
+void draw_line_9(std::vector<sf::Uint8>& frame_buffer);
+void draw_line_10(std::vector<sf::Uint8>& frame_buffer);
+void draw_line_11(std::vector<sf::Uint8>& frame_buffer);
+void draw_line_12(std::vector<sf::Uint8>& frame_buffer);
+void draw_line_13(std::vector<sf::Uint8>& frame_buffer);
+void draw_line_14(std::vector<sf::Uint8>& frame_buffer);
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(w, h), "Line Drawing DDA");
@@ -82,6 +90,11 @@ void draw_line_dda(std::vector<sf::Uint8>& frame_buffer, int x0, int y0, int x1,
     }
     // Horizontal.
     else if (y0 == y1) {
+        // Make ascending.
+        if (x0 > x1) {
+            std::swap(x0, x1);
+        }
+
         for (int x = x0; x <= x1; x++) {
             set_pixel_color(frame_buffer, x, y0, color);
         }
@@ -131,12 +144,20 @@ void draw_line_dda(std::vector<sf::Uint8>& frame_buffer, int x0, int y0, int x1,
 void test_draw_line_dda(std::vector<sf::Uint8>& frame_buffer) {
     draw_axis(frame_buffer);
 
-    //draw_line_1(frame_buffer);
-    //draw_line_2(frame_buffer);
-    //draw_line_3(frame_buffer);  // Должен свопнуть точки.
-    //draw_line_4(frame_buffer);  // Должен свопнуть точки.
-    //draw_line_5(frame_buffer);  // Должен свопнуть точки.
+    draw_line_1(frame_buffer);
+    draw_line_2(frame_buffer);
+    draw_line_3(frame_buffer);  // Должен свопнуть точки.
+    draw_line_4(frame_buffer);  // Должен свопнуть точки.
+    draw_line_5(frame_buffer);  // Должен свопнуть точки.
     draw_line_6(frame_buffer);  // Должен свопнуть точки.
+    draw_line_7(frame_buffer);  // Должен свопнуть точки.
+    draw_line_8(frame_buffer);  // Должен свопнуть точки.
+    draw_line_9(frame_buffer);  // Должен свопнуть точки.
+    draw_line_10(frame_buffer);  // Должен свопнуть точки.
+    draw_line_11(frame_buffer);
+    draw_line_12(frame_buffer);
+    draw_line_13(frame_buffer);
+    draw_line_14(frame_buffer);
 }
 
 void draw_axis(std::vector<sf::Uint8>& frame_buffer) {
@@ -171,4 +192,36 @@ void draw_line_5(std::vector<sf::Uint8>& frame_buffer) {
 
 void draw_line_6(std::vector<sf::Uint8>& frame_buffer) {
     draw_line_dda(frame_buffer, 395, 294, 385, 284, sf::Color::Black);
+}
+
+void draw_line_7(std::vector<sf::Uint8>& frame_buffer) {
+    draw_line_dda(frame_buffer, 399, 298, 387, 292, sf::Color::Black);
+}
+
+void draw_line_8(std::vector<sf::Uint8>& frame_buffer) {
+    draw_line_dda(frame_buffer, 392, 299, 378, 299, sf::Color::Black);
+}
+
+void draw_line_9(std::vector<sf::Uint8>& frame_buffer) {
+    draw_line_dda(frame_buffer, 399, 300, 382, 304, sf::Color::Black);
+}
+
+void draw_line_10(std::vector<sf::Uint8>& frame_buffer) {
+    draw_line_dda(frame_buffer, 395, 304, 388, 311, sf::Color::Black);
+}
+
+void draw_line_11(std::vector<sf::Uint8>& frame_buffer) {
+    draw_line_dda(frame_buffer, 399, 301, 391, 314, sf::Color::Black);
+}
+
+void draw_line_12(std::vector<sf::Uint8>& frame_buffer) {
+    draw_line_dda(frame_buffer, 401, 300, 396, 315, sf::Color::Black);
+}
+
+void draw_line_13(std::vector<sf::Uint8>& frame_buffer) {
+    draw_line_dda(frame_buffer, 401, 301, 410, 321, sf::Color::Black);
+}
+
+void draw_line_14(std::vector<sf::Uint8>& frame_buffer) {
+    draw_line_dda(frame_buffer, 402, 300, 403, 300, sf::Color::Black);
 }
