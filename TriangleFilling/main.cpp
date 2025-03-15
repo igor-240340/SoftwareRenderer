9bcf127973cd4d4e9fc8dc8699d780f420e1e524
igor <igor.240340@gmail.com>
2025-03-15 22:15:38 +0700

    Implement triangle filling
    In progress.

---
+++ TriangleFilling/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+#include <SFML/Graphics.hpp>
+
+constexpr unsigned int w = 800;
+constexpr unsigned int h = 600;
+
+void set_pixel_color(std::vector<sf::Uint8>& frame_buffer, int x, int y, sf::Color color);
+void fill_frame_buffer(std::vector<sf::Uint8>& frame_buffer, sf::Color color);
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(w, h), "Correct Top-Left Triangle Filling");
+
+    std::vector<sf::Uint8> frame_buffer(w * h * 4);
+    fill_frame_buffer(frame_buffer, sf::Color::Magenta);
+
+    sf::Texture texture;
+    if (!texture.create(w, h)) {
+        std::cout << "SFML: Create texture fail.\n";
+    }
+
+    sf::Sprite sprite(texture);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        texture.update(frame_buffer.data());
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
+
+    return 0;
+}
+
+void set_pixel_color(std::vector<sf::Uint8>& frame_buffer, int x, int y, sf::Color color) {
+    const int index = (y * w + x) * 4;
+
+    frame_buffer[index] = color.r;
+    frame_buffer[index + 1] = color.g;
+    frame_buffer[index + 2] = color.b;
+    frame_buffer[index + 3] = color.a;
+}
+
+void fill_frame_buffer(std::vector<sf::Uint8>& frame_buffer, sf::Color color) {
+    for (int i = 0; i < w * h; i++) {
+        const int x = i % w;
+        const int y = i / w;
+        set_pixel_color(frame_buffer, x, y, color);
+    }
+}
