1d152ef8630c203295361e4b01abd382385fe4da
igor <igor.240340@gmail.com>
2025-06-05 03:16:26 +0700

    Implement z-buffer
    Added another test case for z-buffer with larger triangles.
    Wrote some more notes on why it's ok to linearly interpolate z_ndc in screen space.

--- ZBuffer/main.cpp
+++ ZBuffer/main.cpp
@@ -19,12 +19,16 @@ void draw_filled_triangle(std::vector<sf::Uint8>& frame_buffer, std::vector<floa
 void draw_flat_bottom_filled_triangle(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, Vertex v0, Vertex v1, Vertex v2, sf::Color color);
 void draw_flat_top_filled_triangle(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, Vertex v0, Vertex v1, Vertex v2, sf::Color color);
 
-void test_z_buffer(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer);
-void draw_red_triangle(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color = sf::Color::Red);
-void draw_blue_triangle(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color = sf::Color::Blue);
+void test_z_buffer_1(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer);
+void draw_red_triangle_1(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color = sf::Color::Red);
+void draw_blue_triangle_1(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color = sf::Color::Blue);
+
+void test_z_buffer_2(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer);
+void draw_red_triangle_2(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color = sf::Color::Red);
+void draw_blue_triangle_2(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color = sf::Color::Blue);
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(w, h), "Triangle Color Interpolation");
+    sf::RenderWindow window(sf::VideoMode(w, h), "Z-buffer");
 
     sf::Texture texture;
     if (!texture.create(w, h)) {
@@ -44,7 +48,9 @@ int main() {
 
         clear_frame_buffer(frame_buffer, sf::Color::Black);
         clear_z_buffer(z_buffer);
-        test_z_buffer(frame_buffer, z_buffer);
+
+        //test_z_buffer_1(frame_buffer, z_buffer);
+        test_z_buffer_2(frame_buffer, z_buffer);
 
         texture.update(frame_buffer.data());
 
@@ -87,14 +93,14 @@ bool perform_depth_test(std::vector<float>& z_buffer, int frag_x, int frag_y, fl
     return false;
 }
 
-void test_z_buffer(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer) {
-    // NOTE: Мы сразу инициализируем треугольники готовыми значениями в экранных координатах.
+void test_z_buffer_1(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer) {
+    // NOTE: Мы сразу инициализируем треугольники готовыми значениями в экранных координатах (z уже в ndc).
     // Данные референсных треугольников (параметры проекции и пр.) см. в /docs/z_buffer.
-    draw_red_triangle(frame_buffer, z_buffer);
-    draw_blue_triangle(frame_buffer, z_buffer);
+    draw_red_triangle_1(frame_buffer, z_buffer);
+    draw_blue_triangle_1(frame_buffer, z_buffer);
 }
 
-void draw_red_triangle(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color) {
+void draw_red_triangle_1(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color) {
     Vertex v0{ 429.87526371883301015991f, 278.213673882153795979357f, 0.9771141927369544193548f };
     Vertex v1{ 399.5f, 281.423575951731450822185f, 0.984848484848484848475f };
     Vertex v2{ 399.3023924950105377224130415f, 301.0083818992801606481207831f, 0.994719847236195996216935f };
@@ -102,10 +108,33 @@ void draw_red_triangle(std::vector<sf::Uint8>& frame_buffer, std::vector<float>&
     draw_filled_triangle(frame_buffer, z_buffer, v0, v1, v2, color);
 }
 
-void draw_blue_triangle(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color) {
+void draw_blue_triangle_1(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color) {
     Vertex v0{ 410.0021814246378972501077f, 273.2019727100524486681744f, 0.98268269778828361809463f };
     Vertex v1{ 394.1548259360906275421404f, 306.1965571618019583830673f, 0.98296553805218065971735f };
     Vertex v2{ 411.70024765273320929807241f, 298.84448640827966537345991f, 0.99009821541718905839786f };
+
+    draw_filled_triangle(frame_buffer, z_buffer, v0, v1, v2, color);
+}
+
+void test_z_buffer_2(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer) {
+    // NOTE: Мы сразу инициализируем треугольники готовыми значениями в экранных координатах (z уже в ndc).
+    // Данные референсных треугольников (параметры проекции и пр.) см. в docs/z_buffer/test_z_buffer_2.
+    draw_red_triangle_2(frame_buffer, z_buffer);
+    draw_blue_triangle_2(frame_buffer, z_buffer);
+}
+
+void draw_red_triangle_2(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color) {
+    Vertex v0{ 766.5420725179708132653f, 513.4710784732278997712f, 0.982260700923388607925f };
+    Vertex v1{ 411.956887564527930729294f, 468.132227879016286218972f, 0.9816997730122647361999f };
+    Vertex v2{ 455.8765139316101501837f, 158.7993787597697993863f, 0.995852132522469078377f };
+
+    draw_filled_triangle(frame_buffer, z_buffer, v0, v1, v2, color);
+}
+
+void draw_blue_triangle_2(std::vector<sf::Uint8>& frame_buffer, std::vector<float>& z_buffer, sf::Color color) {
+    Vertex v0{ 700.260180421914415334f, 186.0452835278234043394f, 0.983535544185657768075f };
+    Vertex v1{ 300.96275576579127919057f, 256.77482666672850885526f, 0.9941240753837288724578f };
+    Vertex v2{ 604.8665116362462524682f, 553.6157067498294574941f, 0.984758392588700689605f };
 
     draw_filled_triangle(frame_buffer, z_buffer, v0, v1, v2, color);
 }
