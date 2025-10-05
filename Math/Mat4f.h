ff18c658349b994f2ddebb2fa248c99b4711a115
igor <igor.240340@gmail.com>
2025-10-05 16:55:16 +0700

    Implement scaling

--- Math/Mat4f.h
+++ Math/Mat4f.h
@@ -8,24 +8,28 @@
 // Column-major order.
 class Mat4f {
 public:
-    Mat4f();
-    ~Mat4f() = default;
+	Mat4f();
+	~Mat4f() = default;
 
-    Vec4f operator*(const Vec4f& vec) const;
-    Mat4f operator*(const Mat4f& vec) const;
+	Vec4f operator*(const Vec4f& vec) const;
+	Mat4f operator*(const Mat4f& vec) const;
 
-    static Mat4f create_identity();
+	static Mat4f create_identity();
 
-    static Mat4f create_look_at(const Vec3f& pos, const Vec3f& look_dir, const Vec3f& up);
-    static Mat4f create_perspective(float fov_vert_rad, float aspect_ratio, float near, float far);
-    static Mat4f create_ortho(float left, float right, float bottom, float top, float near, float far);
-    static Mat4f create_viewport(int w, int h);
+	static Mat4f create_look_at(const Vec3f& pos, const Vec3f& look_dir, const Vec3f& up);
+	static Mat4f create_perspective(float fov_vert_rad, float aspect_ratio, float near, float far);
+	static Mat4f create_ortho(float left, float right, float bottom, float top, float near, float far);
+	static Mat4f create_viewport(int w, int h);
 
-    static Mat4f create_translation(const Vec3f& v);
-    static Mat4f create_rotation_x(float angle_rad);
-    static Mat4f create_rotation_y(float angle_rad);
-    static Mat4f create_rotation_z(float angle_rad);
+	static Mat4f create_translation(const Vec3f& v);
+	static Mat4f create_rotation_x(float angle_rad);
+	static Mat4f create_rotation_y(float angle_rad);
+	static Mat4f create_rotation_z(float angle_rad);
+
+	static Mat4f create_scale_x(float scale);
+	static Mat4f create_scale_y(float scale);
+	static Mat4f create_scale_z(float scale);
 
 private:
-    std::array<float, 16> data;
+	std::array<float, 16> data;
 };
--- Transformations/main.cpp
+++ Transformations/main.cpp
@@ -1,53 +1,79 @@
 #include <iostream>
 #include <numbers>
 #include <format>
+#include <vector>
 
 #include "Mat4f.h"
 #include "Vec4f.h"
 
 void test_rotate_then_translate();
 void test_translate_then_rotate();
+void test_scale();
 
 float to_radians(float angle_deg);
 
 int main() {
-    test_rotate_then_translate();
-    test_translate_then_rotate();
+	test_rotate_then_translate();
+	test_translate_then_rotate();
+	test_scale();
 
-    return 0;
+	return 0;
 }
 
 // Референсный тестовый пример для сравнения расчётов см. в /docs/transformations/translation/translation.mcdx.
 void test_rotate_then_translate() {
-    const Vec4f vertex_before{ -2.431883860752f, 1.756684629583f, 0.0f, 1.0f };
+	const Vec4f vertex_before{ -2.431883860752f, 1.756684629583f, 0.0f, 1.0f };
 
-    const float beta = to_radians(69.64592296022f);
-    const float gamma = to_radians(46.92909951096f);
-    const float delta = to_radians(76.48913183105f);
-    const Mat4f rot_x = Mat4f::create_rotation_x(gamma);
-    const Mat4f rot_y = Mat4f::create_rotation_y(delta);
-    const Mat4f rot_z = Mat4f::create_rotation_z(beta);
-    const Mat4f translation = Mat4f::create_translation(Vec3f{ 2.2401739117848f, 2.807020326003f, -0.477337272644f });
+	const float beta = to_radians(69.64592296022f);
+	const float gamma = to_radians(46.92909951096f);
+	const float delta = to_radians(76.48913183105f);
+	const Mat4f rot_x = Mat4f::create_rotation_x(gamma);
+	const Mat4f rot_y = Mat4f::create_rotation_y(delta);
+	const Mat4f rot_z = Mat4f::create_rotation_z(beta);
+	const Mat4f translation = Mat4f::create_translation(Vec3f{ 2.2401739117848f, 2.807020326003f, -0.477337272644f });
 
-    const Mat4f transofrmations = translation * rot_y * rot_x * rot_z;
-    const Vec3f vertex_after = transofrmations * vertex_before;
+	const Mat4f transofrmations = translation * rot_y * rot_x * rot_z;
+	const Vec3f vertex_after = transofrmations * vertex_before;
 
-    std::cout << std::format("vertex_after: [{}, {}, {}]\n", vertex_after.x, vertex_after.y, vertex_after.z);
+	std::cout << std::format("vertex_after: [{}, {}, {}]\n", vertex_after.x, vertex_after.y, vertex_after.z);
 }
 
 void test_translate_then_rotate() {
-    const Vec4f vertex_before{ -2.431883860752f, 1.756684629583f, 0.0f, 1.0f };
+	const Vec4f vertex_before{ -2.431883860752f, 1.756684629583f, 0.0f, 1.0f };
 
-    const float beta = to_radians(69.64592296022f);
-    const Mat4f rot_z = Mat4f::create_rotation_z(beta);
-    const Mat4f translation = Mat4f::create_translation(Vec3f{ 2.2401739117848f, 2.807020326003f, -0.477337272644f });
+	const float beta = to_radians(69.64592296022f);
+	const Mat4f rot_z = Mat4f::create_rotation_z(beta);
+	const Mat4f translation = Mat4f::create_translation(Vec3f{ 2.2401739117848f, 2.807020326003f, -0.477337272644f });
 
-    const Mat4f transofrmations = rot_z * translation;
-    const Vec3f vertex_after = transofrmations * vertex_before;
+	const Mat4f transofrmations = rot_z * translation;
+	const Vec3f vertex_after = transofrmations * vertex_before;
 
-    std::cout << std::format("vertex_after: [{}, {}, {}]\n", vertex_after.x, vertex_after.y, vertex_after.z);
+	std::cout << std::format("vertex_after: [{}, {}, {}]\n", vertex_after.x, vertex_after.y, vertex_after.z);
+}
+
+void test_scale() {
+	std::vector<Vec4f> cube_vertices{
+		{ -1.0f, -1.0f, 1.0f, 1.0f },
+		{ -1.0f, -1.0f, -1.0f, 1.0f },
+		{ 1.0f, -1.0f, -1.0f, 1.0f },
+		{ 1.0f, -1.0f, 1.0f, 1.0f },
+		{ -1.0f, 1.0f, 1.0f, 1.0f },
+		{ -1.0f, 1.0f, -1.0f, 1.0f },
+		{ 1.0f, 1.0f, -1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+	};
+
+	Mat4f scale_x = Mat4f::create_scale_x(2.0f);
+	Mat4f scale_y = Mat4f::create_scale_y(0.5f);
+	Mat4f scale_z = Mat4f::create_scale_z(1.5f);
+	Mat4f scale_total = scale_z * scale_y * scale_x;
+
+	for (const Vec4f& v : cube_vertices) {
+		Vec3f vertex_after = scale_total * v;
+		std::cout << std::format("[{}, {}, {}]\n", vertex_after.x, vertex_after.y, vertex_after.z);
+	}
 }
 
 float to_radians(float angle_deg) {
-    return angle_deg * static_cast<float>(std::numbers::pi / 180.0);
+	return angle_deg * static_cast<float>(std::numbers::pi / 180.0);
 }
